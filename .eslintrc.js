39a05dddb253e1588ac01d408796cc3e29c31d17
giho <giho7427@daum.net>
2024-05-30 01:33:42 +0900

    feat: eslint로 크로스 브라우징

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,32 @@ module.exports = {
     es2021: true,
     node: true, // Node.js 환경
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  overrides: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
-  plugins: ['react', 'compat', 'prettier'], // eslint-plugin-compat 추가
+  plugins: ['react', 'compat', 'prettier', 'storybook', '@typescript-eslint'], // eslint-plugin-compat 추가
   rules: {
     'react/react-in-jsx-scope': 'off',
     'compat/compat': 'warn', // 지원하지 않는 ECMAScript, Web API에 대해 경고
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
