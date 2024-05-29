6f6708d16ccd0aba478d682c2e5b81bec681b68e
giho <giho7427@daum.net>
2024-05-29 23:12:31 +0900

    Docs: README 작성 및 웹팩 몇개 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -44,7 +44,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin.CleanWebpackPlugin(),
   ],
   devServer: {
     hot: true,
